8a14961ba4bcfc9209b68c348b9c53b5e0b07caa
Priya Waghmare <113028149+Priya-waghmare@users.noreply.github.com>
2022-11-10 23:20:30 +0530

    Add files via upload

---
+++ emi_calculator.js
@@ -0,0 +1,23 @@
+
+function loan()
+{
+
+    x=Number(document.getElementById('amount').value)
+    y=Number(document.getElementById('interest').value)
+    z=Number(document.getElementById('duration').value)
+
+    
+    //Formula
+    int = (x*(y*0.01))/z
+    res= ((x/z)+int).toFixed(2)
+    amt= (x+(int*z))
+    tot_int= (`${amt}`-`${x}`).toFixed(2)
+
+
+    document.getElementById('emi_val').innerHTML=`Monthly Loan EMI:  &#8377;${res}`
+    document.getElementById('print').innerHTML= `Interest Amount:  &#8377;${tot_int}`
+    document.getElementById('tot_amt').innerHTML=`Total Amount Payable:  &#8377;${amt}`
+    
+
+
+}
